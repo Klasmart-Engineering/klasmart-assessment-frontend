d62b2242fe295d416585eba6bd77efbf4e33a7bb
cheryl <101783623+cherylgithub@users.noreply.github.com>
2022-06-22 12:42:00 +0800

    feat(KLA-522): fix garbled code (#55)
    (cherry picked from commit 9b0d8a6884191c8a5908bc083ba922ad3cdc4744)
    
    Co-authored-by: chery <cheryl.wang@badanamu.com.cn>

--- src/components/UploadCSV/index.tsx
+++ src/components/UploadCSV/index.tsx
@@ -1,82 +1,7 @@
 import { Button } from "@material-ui/core";
+import * as iconv from "iconv-lite";
 import { ChangeEvent } from "react";
-
-
-// 只处理逗号列分隔符
-const COLUMN_DELIMITER = ',';
-
-export function csvToArray(csv: string): string[][] {
-  const table = [] as string[][];
-  let row: string[] = [];
-  let cell = '';
-  let openQuote = false;
-  let i = 0;
-  
-  const pushCell = () => {
-    row.push(cell);
-    cell = '';
-  };
-  
-  const pushRow = () => {
-    pushCell();
-    table.push(row);
-    row = [];
-  }
-  // 处理行分隔符和列分隔符
-  const handleSeparator = (idx: number) => {
-    const c = csv.charAt(idx);
-    if (c === COLUMN_DELIMITER) {
-      pushCell();
-    } else if (c === '\r') {
-      if (csv.charAt(idx + 1) === '\n') {
-        i++;
-      }
-      pushRow();
-    } else if (c === '\n') {
-      pushRow();
-    } else {
-      return false;
-    }
-    return true;
-  }
-  
-  while (i < csv.length) {
-    const c = csv.charAt(i);
-    const next = csv.charAt(i + 1);
-    if (!openQuote && !cell && c === '"') {
-      // 遇到单元第一个字符为双引号时假设整个单元都是被双引号括起来
-        openQuote = true;
-    } else if (openQuote) {
-      // 双引号还未成对的时候
-      if (c !== '"') {
-        // 如非双引号，直接添加进单元内容
-        cell += c;
-      } else if (next === '"') {
-        // 处理双引号转义
-        cell += c;
-        i++;
-      } else {
-        // 确认单元结束
-        openQuote = false
-        if (!handleSeparator(++i)){
-          throw new Error('Wrong CSV format!');
-        }
-      }
-    } else if (!handleSeparator(i)) {
-      // 没有双引号包起来时，如非行列分隔符，一律直接加入单元内容
-      cell += c;
-    }
-    i++;
-  }
-  if (cell) {
-    pushRow();
-  }
-  if(row.length) {
-    pushRow();
-  }
-  return table;
-}
-
+import { csvToArray, isUTF8 } from "./handledata";
 
 export interface CSVObjProps {
   [key: string]: any
@@ -101,10 +26,19 @@ export function UploadCSV(props: UploadCSVProps) {
       } else {
         return new Promise((resolve) => {
           const reader = new FileReader();
-          reader.readAsText(rawFile);
-          reader.onload = e => {
-            const data = e.target?.result as string;
-            const array = csvToArray(data);
+          const reader2 = new FileReader();
+          reader2.readAsArrayBuffer(rawFile);
+          reader2.onload = e => {
+            let originData = e.target?.result as string;
+            const data = Buffer.from(e.target?.result as string);
+            const utf8Data = new Uint8Array(data);
+            const isUtf8 = isUTF8(utf8Data)
+            if(isUtf8) {
+              originData = iconv.decode(data, "utf-8")
+            } else {
+              originData = iconv.decode(data, "gbk")
+            }
+            const array = csvToArray(originData);
             const headers = array[0].filter(i => !!i);
             const contentsArray: CSVObjProps[] = [];
             array.filter(item => !!(Object.values(item).join("")))
