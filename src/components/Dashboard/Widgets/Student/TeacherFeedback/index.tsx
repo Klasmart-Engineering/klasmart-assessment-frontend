0b517a1e1a981b13eea0862923ed2752e7319cae
pancraslu <106637410+pancrasLu@users.noreply.github.com>
2022-06-27 11:32:38 +0800

    Dev/kla 520 bug (#63)
    * feat(KLA-520): fixed the console bug
    
    * feat(KLA-520): fixed the console bug
    
    * feat(KLA-520): fixed the console bug
    
    * feat(KLA-520): fixed the console bug
    
    * feat(KLA-520): fixed the console bug
    
    * feat(KLA-520): bug fix
    
    * feat(KLA-520): style isolation
    
    Co-authored-by: 358881374@qq.com <lpclv6.67>
    Co-authored-by: James.Jiang <james.jiang@badanumu.com.cn>

---
+++ src/components/Dashboard/Widgets/Student/TeacherFeedback/index.tsx
@@ -0,0 +1,10 @@
+import CssProvider from "@components/CssProvider";
+import TeacherFeedbackWidget from "./TeacherFeedbackWidget";
+
+export default function Index() {
+  return (
+    <CssProvider>
+      <TeacherFeedbackWidget />
+    </CssProvider>
+  );
+}
